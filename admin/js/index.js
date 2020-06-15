dd57e3f3cd1944d51bae7b37f7abd615cae12e8a
傅雪涛 <1403664294@qq.com>
2020-06-15 20:31:50 +0800

    菜单栏 （js）

--- admin/js/index.js
+++ admin/js/index.js
@@ -19,4 +19,22 @@ $(function () {
       window.location.href = './login.html';
     }
   });
-})
+
+  // 一级菜单点击事件
+  $('div.level01').on('click', function () {
+    $(this).addClass('active').siblings('div').removeClass('active');
+    if ($(this).index() == 1) {
+      $('ul.level02').slideToggle();
+      $(this).find('b').toggleClass('rotate0');
+      $('ul.level02>li:eq(0)>a')[0].click();
+      //jQuery对象的click()事件,他只会触发js单击事件,而不会触发a标签的默认跳转事件.
+      //dom对象的click()事件,他不仅会触发js单击事件,还会触发a标签的默认跳转事件
+    }
+  });
+
+  // 二级菜单点击事件
+  $('ul.level02>li').on('click', function () {
+    $(this).parent().siblings('div').removeClass('active');
+    $(this).addClass('active').siblings('li').removeClass('active');
+  });
+});
