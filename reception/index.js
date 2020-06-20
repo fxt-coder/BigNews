aa258a9c04d17791e69becba92c9a6abb10c9f5e
傅雪涛 <1403664294@qq.com>
2020-06-20 17:57:04 +0800

    前台主页

--- reception/index.js
+++ reception/index.js
@@ -23,16 +23,83 @@ $(function () {
       }
     }
   });
+
+
+  // 三：热门排行
+
+  $.ajax({
+    url: 'http://localhost:8080/api/v1/index/rank',
+    success: function (backData) {
+      // console.log(backData);
+      if (backData.code == 200) {
+        for (var i = 0; i < backData.data.length; i++) {
+          $('.hotrank_list>li').eq(i).children('a').text(backData.data[i].title);
+          $('.hotrank_list>li').eq(i).children('a').attr('href', './article.html?id=' + backData.data[i].id);
+        }
+      }
+    }
+  });
+
+  // 四：最新评论
+  $.ajax({
+    url: 'http://localhost:8080/api/v1/index/latest_comment',
+    success: function (backData) {
+      // console.log(backData);
+      var month = new Date().getMonth() + 1;
+      backData.month = month;
+      var resHtml = template('lastecomment', backData);
+      $('.comment_list').html(resHtml);
+    }
+  })
+
+  // 五：焦点关注
+  $.ajax({
+    url: 'http://localhost:8080/api/v1/index/attention',
+    success: function (backData) {
+      // console.log(backData);
+      if (backData.code == 200) {
+        var resHtml = template('attention', backData);
+        $('.guanzhu_list').html(resHtml);
+      }
+    }
+  });
+
+  // 获取文章类别
+  $.ajax({
+    url: 'http://localhost:8080/api/v1/index/category',
+    success: function (backData) {
+      //console.log(backData);
+      if (backData.code == 200) {
+        var resHtml = template('category_temp', backData);
+        $('.level_two').html(resHtml);
+        //七.获取所有的文章类型2
+        var resHtml2 = template('category_temp2', backData);
+        $('.left_menu.fl').html(resHtml2);
+      }
+    }
+  });
+
+
+  // 搜索文章
+  $('.search_btn').on('click', function () {
+    var searchTxt = $('.search_txt').val().trim();
+    if (searchTxt == '') {
+      alert('不能为空');
+      return;
+    } else if (isNaN(parseInt(searchTxt)) == false) {
+      alert('搜索关键词为文本内容');
+      return;
+    } else {
+      window.location.href = './list.html?searchTxt=' + searchTxt;
+    }
+  });
+
+  // 回车搜索
+  $('.search_txt').on('keydown', function (e) {
+    if (e.keyCode == 13) {
+      // alert('点击了一下回车');
+      $('.search_btn').click();
+    }
+  });
 })
 
-// 三：热门排行
-
-$.ajax({
-  type: "method",
-  url: 'http://localhost:8080/api/v1/index/rank',
-  data: "data",
-  dataType: "dataType",
-  success: function (response) {
-    
-  }
-});
